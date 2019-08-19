43186c049865d67ffdc647d7848687181613e0a3
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-08-19 13:45:53 -0400

    Imported to proper Index.js file

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
-import { BrowserRouter as Router} from 'react-router-dom';
 
 
-ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById("root")
-  );
 
-  
 export { Home, About, Contact, Navigation };
 
 
